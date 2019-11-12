cf416f7438f00cf117f93e107e8a56a8f85573f4
akodanka <anoob.anto.kodankandath@intel.com>
2019-11-12 21:12:15 +0530

    Enable audio for CIC
    Changes to enable audio on CIC
    
    Tracked-On: OAM-88517
    Signed-off-by: akodanka <anoob.anto.kodankandath@intel.com>

--- cic_dev/BoardConfig.mk
+++ cic_dev/BoardConfig.mk
@@ -53,5 +53,24 @@ INTEL_STAGEFRIGHT := true
 USE_MEDIASDK := true
 BOARD_HAVE_MEDIASDK_OPEN_SOURCE := true
 
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+BOARD_USES_TINY_ALSA_AUDIO := true
+BOARD_USES_GENERIC_AUDIO ?= false
+USE_CUSTOM_PARAMETER_FRAMEWORK := false
+ifneq ($(BOARD_USES_GENERIC_AUDIO), true)
+# Audio HAL selection Flag default setting.
+#  INTEL_AUDIO_HAL:= audio     -> baseline HAL
+#  INTEL_AUDIO_HAL:= audio_pfw -> PFW-based HAL
+INTEL_AUDIO_HAL := audio
+else
+INTEL_AUDIO_HAL := stub
+endif
+
 # Use XML audio policy configuration file
-USE_XML_AUDIO_POLICY_CONF ?= 1
+USE_XML_AUDIO_POLICY_CONF := 1
+# Use configurable audio policy
+USE_CONFIGURABLE_AUDIO_POLICY := 0
+
+# Use Baseline Legacy Audio HAL
+USE_LEGACY_BASELINE_AUDIO_HAL := true
