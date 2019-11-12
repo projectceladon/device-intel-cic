b6477498a19257ed569d2b03e206632c7859c744
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2019-11-12 10:48:44 +0530

    cic: Add changes for enabling camera
    - Added packages needed for verifying camera
    - Modified external camera config file to support 1080p.
    - Added sepolicy files.
    - Camera2 app changes to support external camera
    - Update media_profiles.xml to support 1080p.
    
    Tracked-On: OAM-88515
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- cic_dev/cic_dev.mk
+++ cic_dev/cic_dev.mk
@@ -25,9 +25,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     device/intel/cic/common/media_codecs.xml:system/vendor/etc/media_codecs.xml \
-    device/intel/cic/common/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    device/intel/cic/common/media_codecs_performance.xml:system/vendor/etc/media_codecs_performance.xml \
     device/intel/cic/common/mfx_omxil_core.conf:system/vendor/etc/mfx_omxil_core.conf \
-    device/intel/cic/common/media_profiles.xml:system/etc/media_profiles.xml \
+    device/intel/cic/common/media_profiles.xml:system/vendor/etc/media_profiles_V1_0.xml \
     device/intel/cic/cic_dev/init.cic_dev.rc:root/init.cic_dev.rc
 
 PRODUCT_COPY_FILES += \
