570f2e8dd1d78766ab3e048aa345abedf7c33462
Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>
2019-11-12 08:09:12 +0530

    Add changes to enable bluetooth in cic
    - Packages needed for bluetooth added
    - Vendor specific inteface details added to manifest.xml
    - Overlay files to override configs for tablet version added
    - sepolicy files added
    
    Tracked-On: OAM-88512
    Signed-off-by: Jeevaka Prabu Badrappan <jeevaka.badrappan@intel.com>

--- common/device.mk
+++ common/device.mk
@@ -136,3 +136,23 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     sdcard-fuse
+
+# Bluetooth tools
+PRODUCT_PACKAGES += \
+    hciconfig \
+
+# Bluetooth tools eng / userdebug
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_PACKAGES += \
+    hcitool
+endif
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-service.vbt \
+    libbt-vendor
+
+PRODUCT_PACKAGE_OVERLAYS += device/intel/cic/common/bluetooth/tablet/overlay
