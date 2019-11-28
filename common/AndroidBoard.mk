283ee29fb66a29545ce4ff9861252ac83d6528d6
sgnanase <sundar.gnanasekaran@intel.com>
2019-11-28 12:21:16 +0530

    [CIC] Make .deb release along with flashfiles
    Tracked-On: OAM-88675
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- common/AndroidBoard.mk
+++ common/AndroidBoard.mk
@@ -31,8 +31,8 @@ TARGET_AIC_FILE_NAME := $(TARGET_PRODUCT)-aic-$(BUILD_NUMBER).tar.gz
 addon:
 ifeq ($(TARGET_PRODUCT), cic_dev)
 	@echo Make additional release binaries/files...
-	$(hide) rm -rf $(PRODUCT_OUT)/cfc $(PRODUCT_OUT)/pre-requisites  $(PRODUCT_OUT)/README-CIC  $(PRODUCT_OUT)/setup-aic
-	$(hide) cp -r $(TOP)/device/intel/cic/$(TARGET_PRODUCT)/addon/* $(TOP)/vendor/intel/cic/host/cfc $(PRODUCT_OUT)/.
+	$(hide) rm -rf $(PRODUCT_OUT)/cfc $(PRODUCT_OUT)/cic
+	$(hide) cp -r $(TOP)/vendor/intel/cic/host/cfc $(PRODUCT_OUT)/.
 else
 	@echo Nothing todo
 endif
@@ -47,7 +47,13 @@ else
 	BUILD_VARIANT=loop_mount $(HOST_OUT_EXECUTABLES)/aic-build -b $(BUILD_NUMBER)
 endif
 ifeq ($(TARGET_PRODUCT), cic_dev)
-	tar cvzf $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) -C $(PRODUCT_OUT) aic android.tar.gz aic-manager.tar.gz cfc pre-requisites README-CIC setup-aic -C docker update
+	tar cvzf $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) -C $(PRODUCT_OUT) aic android.tar.gz aic-manager.tar.gz cfc -C docker update
+	$(hide) rm -rf $(PRODUCT_OUT)/cic
+	$(hide) mkdir -p $(PRODUCT_OUT)/cic/cic
+	$(hide) (cd $(PRODUCT_OUT)/cic/cic && tar xvf ../../$(TARGET_AIC_FILE_NAME) aic android.tar.gz aic-manager.tar.gz cfc update)
+	$(hide) mkdir -p $(PRODUCT_OUT)/cic/DEBIAN
+	$(hide) cp -r device/intel/cic/$(TARGET_PRODUCT)/addon/* $(PRODUCT_OUT)/cic/DEBIAN/.
+	$(hide) (cd $(PRODUCT_OUT)/ && dpkg-deb --build cic/)
 else
 	tar cvzf $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) -C $(PRODUCT_OUT) aic android.tar.gz aic-manager.tar.gz -C docker update
 endif
@@ -60,6 +66,8 @@ publish_ci: aic
 	@echo Publish CI AIC docker images...
 	$(hide) mkdir -p $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
 	$(hide) cp $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
+	$(hide) cp $(PRODUCT_OUT)/*.deb $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
+	$(hide) cp $(PRODUCT_OUT)/cfc/* $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
 
 # Following 1A CI practice, "publish" is used by buildbot for "latest", "release", etc. Without this
 # target, the build will fail on related buildbot.
@@ -69,3 +77,5 @@ publish: aic
 	@echo Publish AIC docker images...
 	$(hide) mkdir -p $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
 	$(hide) cp $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
+	$(hide) cp $(PRODUCT_OUT)/*.deb $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
+	$(hide) cp $(PRODUCT_OUT)/cfc/* $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
