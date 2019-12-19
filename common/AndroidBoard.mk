0aa1577a443317ea78fb66666cf4d25a045145e1
sgnanase <sundar.gnanasekaran@intel.com>
2019-12-20 05:29:59 +0530

    CIC/CFC release package should be deb not tar ball
    Tracked-On: OAM-88919
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- common/AndroidBoard.mk
+++ common/AndroidBoard.mk
@@ -29,7 +29,7 @@ endif
 TARGET_AIC_FILE_NAME := $(TARGET_PRODUCT)-aic-$(BUILD_NUMBER).tar.gz
 
 .PHONY: addon
-addon:
+addon: 
 ifeq ($(TARGET_PRODUCT), cic_dev)
 	@echo Make additional release binaries/files...
 	$(hide) rm -rf $(PRODUCT_OUT)/cfc $(PRODUCT_OUT)/pre-requisites  $(PRODUCT_OUT)/README-CIC  $(PRODUCT_OUT)/setup-aic
@@ -49,6 +49,13 @@ else
 endif
 ifeq ($(TARGET_PRODUCT), cic_dev)
 	tar cvzf $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) -C $(PRODUCT_OUT) aic android.tar.gz aic-manager.tar.gz cfc pre-requisites README-CIC setup-aic -C docker update
+	@echo Make debian binaries...
+	$(hide) (rm -rf $(PRODUCT_OUT)/cic && mkdir -p $(PRODUCT_OUT)/cic/opt/cic)
+	$(hide) (cd $(PRODUCT_OUT)/cic/opt/cic && tar xvf ../../../$(TARGET_AIC_FILE_NAME) aic android.tar.gz aic-manager.tar.gz cfc update)
+	$(hide) mkdir -p $(PRODUCT_OUT)/cic/DEBIAN
+	$(hide) cp -r device/intel/cic/$(TARGET_PRODUCT)/addon/debian/* $(PRODUCT_OUT)/cic/DEBIAN/.
+	$(hide) dpkg -x $(PRODUCT_OUT)/cfc/cfc_0.1.0_x64.deb $(PRODUCT_OUT)/cic/.
+	$(hide) (cd $(PRODUCT_OUT)/ && dpkg-deb --build cic/)
 else
 	tar cvzf $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) -C $(PRODUCT_OUT) aic android.tar.gz aic-manager.tar.gz -C docker update
 endif
@@ -61,6 +68,7 @@ publish_ci: aic
 	@echo Publish CI AIC docker images...
 	$(hide) mkdir -p $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
 	$(hide) cp $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
+	$(hide) cp $(PRODUCT_OUT)/cic.deb $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
 
 # Following 1A CI practice, "publish" is used by buildbot for "latest", "release", etc. Without this
 # target, the build will fail on related buildbot.
@@ -70,3 +78,4 @@ publish: aic
 	@echo Publish AIC docker images...
 	$(hide) mkdir -p $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
 	$(hide) cp $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
+	$(hide) cp $(PRODUCT_OUT)/cic.deb $(TOP)/pub/$(TARGET_PRODUCT)/$(TARGET_BUILD_VARIANT)
