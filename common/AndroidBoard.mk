c3987f5b78a78dfa9e98c4d160a7e24316ae9949
sgnanase <sundar.gnanasekaran@intel.com>
2020-03-06 14:49:37 +0530

    [CIC] Add license file as part of release package
    Tracked-On: OAM-89798
    Signed-off-by: sgnanase <sundar.gnanasekaran@intel.com>

--- common/AndroidBoard.mk
+++ common/AndroidBoard.mk
@@ -32,7 +32,7 @@ TARGET_AIC_FILE_NAME := $(TARGET_PRODUCT)-aic-$(BUILD_NUMBER).tar.gz
 addon: 
 ifeq ($(TARGET_PRODUCT), cic_dev)
 	@echo Make additional release binaries/files...
-	$(hide) rm -rf $(PRODUCT_OUT)/cfc $(PRODUCT_OUT)/pre-requisites  $(PRODUCT_OUT)/README-CIC  $(PRODUCT_OUT)/ia_hwc $(PRODUCT_OUT)/setup-aic
+	$(hide) rm -rf $(PRODUCT_OUT)/cfc $(PRODUCT_OUT)/pre-requisites  $(PRODUCT_OUT)/README-CIC  $(PRODUCT_OUT)/ia_hwc $(PRODUCT_OUT)/setup-aic $(PRODUCT_OUT)/LICENSE
 	$(hide) cp -r $(TOP)/device/intel/cic/$(TARGET_PRODUCT)/addon/* $(TOP)/vendor/intel/cic/host/cfc $(TOP)/vendor/intel/cic/target/graphics/ia_hwc $(PRODUCT_OUT)/.
 	$(hide) cp $(TOP)/device/intel/cic/cic_dev/addon/cic.sh $(PRODUCT_OUT)
 else
@@ -49,10 +49,10 @@ else
 	BUILD_VARIANT=loop_mount $(HOST_OUT_EXECUTABLES)/aic-build -b $(BUILD_NUMBER)
 endif
 ifeq ($(TARGET_PRODUCT), cic_dev)
-	tar cvzf $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) -C $(PRODUCT_OUT) aic android.tar.gz aic-manager.tar.gz cfc ia_hwc pre-requisites README-CIC cic.sh setup-aic -C docker update
+	tar cvzf $(PRODUCT_OUT)/$(TARGET_AIC_FILE_NAME) -C $(PRODUCT_OUT) aic android.tar.gz aic-manager.tar.gz cfc ia_hwc pre-requisites README-CIC LICENSE cic.sh setup-aic -C docker update
 	@echo Make debian binaries...
 	$(hide) (rm -rf $(PRODUCT_OUT)/cic && mkdir -p $(PRODUCT_OUT)/cic/opt/cic && mkdir -p $(PRODUCT_OUT)/cic/etc/profile.d)
-	$(hide) (cd $(PRODUCT_OUT)/cic/opt/cic && tar xvf ../../../$(TARGET_AIC_FILE_NAME) aic android.tar.gz aic-manager.tar.gz cic.sh cfc update)
+	$(hide) (cd $(PRODUCT_OUT)/cic/opt/cic && tar xvf ../../../$(TARGET_AIC_FILE_NAME) aic android.tar.gz aic-manager.tar.gz LICENSE cic.sh cfc update)
 	$(hide) mkdir -p $(PRODUCT_OUT)/cic/DEBIAN
 	$(hide) cp -r device/intel/cic/$(TARGET_PRODUCT)/addon/debian/* $(PRODUCT_OUT)/cic/DEBIAN/.
 	$(hide) cp -r device/intel/cic/$(TARGET_PRODUCT)/addon/pre-requisites/create_pasocket.sh $(PRODUCT_OUT)/cic/etc/profile.d
